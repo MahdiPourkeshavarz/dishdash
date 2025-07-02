49d34debf1de15cc1cb1f8d30b269e9777f34de0
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-07-02 11:59:59 +0330

    add(tailwind): set up for project main style

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        brand: {
+          DEFAULT: "#4338CA", // indigo-700
+          primary: "#4338CA", // indigo-700
+          secondary: "#3730A3", // indigo-800
+        },
+        success: "#16A34A", // green-600
+        warning: "#D97706", // amber-600
+        danger: "#DC2626", // red-600
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
+export default config;
