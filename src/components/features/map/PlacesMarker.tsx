af3b2314a7c246cbb003c62df5f62d064e6d90e1
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-07-27 16:39:10 +0330

    feat(poi marker): added places name to the marker component

--- src/components/features/map/PlacesMarker.tsx
+++ src/components/features/map/PlacesMarker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMemo } from "react";
-import { Marker } from "react-leaflet";
+import { Marker, Tooltip, useMap } from "react-leaflet";
 import L from "leaflet";
 import { useStore } from "@/store/useStore";
 import { Poi } from "@/types";
@@ -12,28 +12,29 @@ interface PoiMarkersLayerProps {
 
 export const PlacesMarker: React.FC<PoiMarkersLayerProps> = ({ pois }) => {
   const { theme, setSelectedPoi, setHighlightedPoi } = useStore();
+  const map = useMap();
 
   const restaurantIcon = L.icon({
     iconUrl: theme === "light" ? "./restaurants.png" : "./restaurants-dark.png",
     iconSize: theme === "light" ? [30, 32] : [27, 36],
-    iconAnchor: [16, 16],
-    popupAnchor: [0, -16],
+    iconAnchor: [15, 32],
+    popupAnchor: [0, 0],
     className: "map-poi-icon",
   });
 
   const cafeIcon = L.icon({
     iconUrl: theme === "light" ? "./cafe.png" : "./cafe-dark.png",
     iconSize: theme === "light" ? [30, 32] : [27, 36],
-    iconAnchor: [16, 16],
-    popupAnchor: [0, -16],
+    iconAnchor: [15, 32],
+    popupAnchor: [0, 0],
     className: "map-poi-icon",
   });
 
   const fastfoodIcon = L.icon({
     iconUrl: theme === "light" ? "./fastfood.png" : "./fastfood-dark.png",
     iconSize: theme === "light" ? [30, 33] : [27, 36],
-    iconAnchor: [16, 16],
-    popupAnchor: [0, -16],
+    iconAnchor: [15, 32],
+    popupAnchor: [0, 0],
     className: "map-poi-icon",
   });
 
@@ -64,6 +65,9 @@ export const PlacesMarker: React.FC<PoiMarkersLayerProps> = ({ pois }) => {
             return null;
         }
 
+        const currentZoom = map.getZoom();
+        const showlabel = currentZoom >= 18;
+
         return (
           <Marker
             key={keyId + poi.tags.name}
@@ -75,7 +79,21 @@ export const PlacesMarker: React.FC<PoiMarkersLayerProps> = ({ pois }) => {
                 setHighlightedPoi(poi.id);
               },
             }}
-          />
+          >
+            {showlabel && (
+              <Tooltip
+                direction="top"
+                offset={[43, -30]}
+                opacity={1}
+                permanent
+                className={`custom-label ${
+                  theme === "dark" ? "dark-theme" : "light-theme"
+                }`}
+              >
+                <span>{poi.tags.name}</span>
+              </Tooltip>
+            )}
+          </Marker>
         );
       })
       .filter(Boolean);
