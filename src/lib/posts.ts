2a01b6a4ded569f44f2598c42edcb69a2916eb93
Mahdi Pourkeshavarz <69865082+MahdiPourkeshavarz@users.noreply.github.com>
2025-08-09 17:54:12 +0330

    Update posts.ts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,15 +2,32 @@ import { Post } from "@/types";
 
 export const posts: Post[] = [
   {
-    user: {
-      id: "123",
-      username: "محمد محمدی",
-      imgUrl: "/user-photo.jpg",
-    },
-    description: "عجب غذایی بود خیلی خوش گذشت",
-    id: "",
+    _id: "68930d19b1f94414a1e0fe26",
+    description: "کباب و جیگر هاش خوب بود",
+    imageUrl: "https://res.cloudinary.com/dcr7qgyew/image/upload/v1754467607/dishdash_posts/kw3x0f3tnhf3jgcy5juo.jpg",
+    position: [51.3118336, 35.6119359],
+    areaName: "کبابسرای چهار دانگه",
     satisfaction: "awesome",
-    imageUrl: "/food.webp",
-    position: [35.6892, 51.389],
+    likes: 1,
+    dislikes: 0,
+    userId: "68930b26b1f94414a1e0fe25",
+    place: {
+      _id: "68809061ad65c6cc6e728bb6",
+      osmId: 10591628790,
+      name: "کبابسرای چهار دانگه",
+      position: [51.3118336, 35.6119359],
+      tags: {
+        "addr:street": "بزرگراه آیت الله سعیدی",
+        amenity: "restaurant",
+        name: "کبابسرای چهار دانگه",
+        opening_hours: "Mo-Su 12:00-24:00",
+        phone: "+98 21 5528 3908",
+      },
+      averageRating: 4,
+      ratingCount: 1,
+    },
+    createdAt: new Date("2025-08-06T08:06:49.952Z"),
+    placeId: "68809061ad65c6cc6e728bb6",
   },
 ];
+
